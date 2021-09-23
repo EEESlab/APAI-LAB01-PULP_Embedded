6bda0ad95176a412e375c3c650aaa4d0ebc1211b
hejleo <59688721+hejleo@users.noreply.github.com>
2021-09-23 18:07:29 +0200

    Delete pulp-examples directory

--- pulp-examples/BUILD/PULP/GCC_RISCV/test/kernel/irq_asm.d
+++
@@ -1,8 +0,0 @@
-/home/pulp-user/APAI-LAB01-PULP_Embedded/pulp-examples/BUILD/PULP/GCC_RISCV//test/kernel/irq_asm.o: \
- /pulp/pulp-sdk/rtos/pulpos/common/kernel/irq_asm.S \
- /pulp/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h \
- /pulp/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h
-
-/pulp/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h:
-
-/pulp/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h:
