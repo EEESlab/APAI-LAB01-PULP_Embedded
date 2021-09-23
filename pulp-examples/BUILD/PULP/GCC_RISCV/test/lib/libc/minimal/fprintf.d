869fd37748d49be58c2ce1a84effd173737ff598
Leonardo Ravaglia <leonardoravaglia94@gmail.com>
2021-09-23 17:25:53 +0200

    added matrix element search example

---
+++ pulp-examples/BUILD/PULP/GCC_RISCV/test/lib/libc/minimal/fprintf.d
@@ -0,0 +1,11 @@
+/home/pulp-user/APAI-LAB01-PULP_Embedded/pulp-examples/BUILD/PULP/GCC_RISCV//test/lib/libc/minimal/fprintf.o: \
+ /pulp/pulp-sdk/rtos/pulpos/common/lib/libc/minimal/fprintf.c \
+ /pulp/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h \
+ /pulp/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h \
+ /pulp/pulp-sdk/rtos/pulpos/common/lib/libc/minimal/include/stdio.h
+
+/pulp/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h:
+
+/pulp/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h:
+
+/pulp/pulp-sdk/rtos/pulpos/common/lib/libc/minimal/include/stdio.h:
