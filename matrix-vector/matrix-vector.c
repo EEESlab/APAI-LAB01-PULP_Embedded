d159e2d7f86248a59dc42e9e81842eb50b226b5b
hejleo <59688721+hejleo@users.noreply.github.com>
2021-09-24 18:16:58 +0200

    added comments

--- matrix-vector/matrix-vector.c
+++ matrix-vector/matrix-vector.c
@@ -9,11 +9,12 @@
  */
 #include "pmsis.h"
 
-
+// generic matrix-vector multiplication
 int gemv(int N, int M, float * mat, float *vec, float * output_vec){
 
     for (int i=0; i<N; i++){
       for (int j=0; j<M; j++){
+          // vectors are scrolled using a single index, while the matrix (which is still defined as a contiguous array) is scanned using two indeces
           *(output_vec+i) += *(mat+i*M+j)*(*(vec+j));
       }
     }
@@ -24,15 +25,20 @@ int gemv(int N, int M, float * mat, float *vec, float * output_vec){
 int main()
 {
   int N = 5, M = 6;
+  // we define the matrix as an array of size N*M
   float matrix[N*M];
+  // we initialize the matrix as we wish, for example:
   for (int i=0; i<30; i++) { matrix[i] = i; }
 
+  // the second vector operand is defined and initialized:
   float vector[M];
   for (int i=0; i<M; i++) { vector[i] = 10-i; }
 
+  // we define also the output, and initialize it to 0s:
   float output[N];
   for (int i=0; i<N; i++) { output[i] = 0.0f; }
 
+  // the matrix-vector product is performed and printed out:
   gemv(N, M, matrix, vector, output);
   for (int i=0; i<N; i++) { printf("%f \n", output[i]); }
 
